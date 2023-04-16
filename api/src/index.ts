0c97cb8e25ac8cc9fa48e4f776299f0c6fadb41a
Abner Lima <abnerlima.1997@gmail.com>
2023-04-16 17:33:54 -0300

    Configurando o Prisma ORM

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,14 +1,11 @@
 import express from "express";
 import { router } from "./routes";
-import sequelize from "./sequelize";
 
 export const app = express();
 const PORT = 3000;
 
 app.use(router);
 
-sequelize.sync().then(() => {
-  app.listen(PORT, () => {
-    console.log(`NTask API - porta ${PORT}.`);
-  });
+app.listen(PORT, () => {
+  console.log(`NTask API - porta ${PORT}.`);
 });
--- api/src/routes/tasks.ts
+++ api/src/routes/tasks.ts
@@ -1,12 +1,12 @@
-import express from 'express';
+import { prisma } from "../prisma";
+import express from "express";
 
 export const tasksRouter = express.Router();
 
-tasksRouter.get("/tasks", (_, res) => {
-    res.json({
-        tasks: [
-            { title: "Fazer compras" },
-            { title: "Consertar o PC" }
-        ]
-    });
+tasksRouter.get("/tasks", async (_, res) => {
+  const tasks = await prisma.task.findMany();
+
+  res.json({
+    tasks: tasks,
+  });
 });
